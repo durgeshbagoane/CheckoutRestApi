bd3a2c6c0d1994a497126c21861a5b89451c2d27
Durgesh Bagoane <durgeshbaraiya98@gmail.com>
2024-01-19 21:08:47 +0000

    initial commit

--- obj/Debug/net8.0/CheckoutRestApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/CheckoutRestApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckoutRestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d26187ec34d659265a634df63ae82d93644db2e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckoutRestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckoutRestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
